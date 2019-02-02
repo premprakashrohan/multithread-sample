fbcd05ef84e2f06d388ad6b35df06a5ec0e48624
premprakashrohan <premprakashrohan@gmail.com>
2019-02-02 15:25:07 +0530

    test

---
+++ src/main/java/multithread/TimeOutThread.java
@@ -0,0 +1,20 @@
+package multithread;
+
+public class TimeOutThread {
+
+	public static void main(String[] args) throws InterruptedException {
+		Thread t1 = new Thread(() -> {
+			 
+				System.out.println(Thread.currentThread());
+				if(Thread.currentThread().isInterrupted()) {
+					System.out.println("Stoping...");
+					return;
+				}
+			
+		});
+		t1.start();
+		t1.wait(11111);
+		t1.interrupt();
+	}
+
+}
